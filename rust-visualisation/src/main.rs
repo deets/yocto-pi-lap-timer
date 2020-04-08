bce2654f9f1e3ec321340a4f067b900b31b3dd9f
Diez B. Roggisch <deets@web.de>
2020-04-08 15:26:48 +0200

    Use message passing to count the number of messages received

--- rust-visualisation/src/main.rs
+++ rust-visualisation/src/main.rs
@@ -1,5 +1,8 @@
 use std::thread;
+use std::sync::mpsc;
+
 use nannou::prelude::*;
+
 use nanomsg::{Socket, Protocol, Error};
 use std::io::{self, Read};
 
@@ -26,36 +29,63 @@ fn connect_to_spi_daemon(addr: &str) -> Result<nanomsg::Socket, Error> {
 
 
 fn main() {
-    let uri = "tcp://fpv-laptimer.local:5000";
-    let mut socket = connect_to_spi_daemon(uri).unwrap();
-    let handle = thread::spawn(move || {
-        loop {
-            let mut buffer = Vec::new();
-            match socket.read_to_end(&mut buffer) {
-                Ok(_) => {
-                    println!("Read message {} bytes !", buffer.len());
-                }
-                Err(err) => {
-                    panic!("Problem reading from socket: {}", err);
-                }
-            }
-        }
-    });    
     nannou::app(model)
         .update(update)
         .simple_window(view)
         .run();
 }
 
-struct Model {}
+struct Model {
+    message_count: u64,
+    incoming_rx: std::sync::mpsc::Receiver<u64>
+
+}
 
 fn model(_app: &App) -> Model {
-    Model {}
+    let uri = "tcp://fpv-laptimer.local:5000";
+    let mut socket = connect_to_spi_daemon(uri).unwrap();
+    let (tx, rx) = mpsc::channel();
+
+    let handle = thread::spawn(move || {
+        loop {
+            let mut buffer = Vec::new();
+            match socket.read_to_end(&mut buffer) {
+                Ok(_) => {
+                    println!("Read message {} bytes !", buffer.len());
+                    tx.send(1);
+                }
+                Err(err) => {
+                    panic!("Problem reading from socket: {}", err);
+                }
+            }
+        }
+    });
+
+    Model {
+        message_count: 0,
+        incoming_rx: rx
+    }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    match model.incoming_rx.try_recv()
+    {
+        Ok(number) => {     model.message_count += number; }
+        Err(_) => {}
+    }
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame){
+fn view(app: &App, model: &Model, frame: Frame){
+    let draw = app.draw();
     frame.clear(PURPLE);
+    // We'll align to the window dimensions, but padded slightly.
+    let win_rect = app.main_window().rect().pad(20.0);
+    draw.background().color(PURPLE);
+
+    let text = format!("message count: {}", model.message_count);
+
+    draw.text(&text).color(WHITE).font_size(24).wh(win_rect.wh());
+
+    draw.to_frame(app, &frame).unwrap();
+
 }
